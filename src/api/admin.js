102f5d0f8a78ac80b06bdb985a551cb692409baf
jemiezler <142890376+jemiezler@users.noreply.github.com>
2024-05-13 00:27:35 +0700

    commit ja

--- src/api/admin.js
+++ src/api/admin.js
@@ -6,8 +6,11 @@ const pool = require('../config/pool');
 
 const { router: billsRouter } = require('./features/bills');
 const { router: announcementsRouter } = require('./features/announcement');
+const { router: maintenanceRouter } = require('./features/maintenance');
+const { router: usermanagementRouter } = require('./features/usermanagement');
 router.use('/bills', billsRouter);
 router.use('/announcements', announcementsRouter);
-
+router.use('/maintenance', maintenanceRouter);
+router.use('/organization', usermanagementRouter);
 
 module.exports = { router }; 
--- src/api/sys.js
+++ src/api/sys.js
@@ -4,6 +4,78 @@ const mysql = require('mysql2/promise');
 const uuid = require('uuid');
 const pool = require('../config/pool');
 
+// FUNCTION
+async function createOrganizationTables(org_code) {
+    const roomTableName = `${org_code}_room`;
+    const createRoomTableQuery = `CREATE TABLE ${roomTableName} (
+        id VARCHAR(10) PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        description TEXT,
+        is_active ENUM('yes', 'no') DEFAULT 'yes'
+    )`;
+    await pool.promise().query(createRoomTableQuery);
+
+
+    const billTableName = `${org_code}_bill`;
+    const createBillTableQuery = `CREATE TABLE ${billTableName} (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT,
+        cost DOUBLE,
+        is_paid ENUM('yes', 'no') DEFAULT 'no',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        room_id VARCHAR(10),
+        customer_id VARCHAR(36),
+        informant_id VARCHAR(36),
+        FOREIGN KEY (room_id) REFERENCES ${roomTableName}(id),
+        FOREIGN KEY (informant_id) REFERENCES _User(id),
+        FOREIGN KEY (customer_id) REFERENCES _User(id)
+    )`;
+    await pool.promise().query(createBillTableQuery);
+
+    const announcementTableName = `${org_code}_announcement`;
+    const createAnnouncementTableQuery = `CREATE TABLE ${announcementTableName} (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description TEXT,
+            is_expired ENUM('yes', 'no') DEFAULT 'no',
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            informant_id VARCHAR(36),
+            FOREIGN KEY (informant_id) REFERENCES _User(id)
+        )`;
+    await pool.promise().query(createAnnouncementTableQuery);
+
+    const maintenanceTableName = `${org_code}_maintenance`;
+    const createMaintenanceTableQuery = `CREATE TABLE ${maintenanceTableName} (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            title VARCHAR(255) NOT NULL,
+            description TEXT,
+            is_fixed ENUM('yes', 'no') DEFAULT 'no',
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            informant_id VARCHAR(36),
+            room_id VARCHAR(10),
+            FOREIGN KEY (informant_id) REFERENCES _User(id),
+            FOREIGN KEY (room_id) REFERENCES ${roomTableName}(id)
+        )`;
+    await pool.promise().query(createMaintenanceTableQuery);
+
+    const mailTableName = `${org_code}_mail`;
+    const createMailTableQuery = `CREATE TABLE ${mailTableName} (
+        id INT AUTO_INCREMENT PRIMARY KEY,
+        title VARCHAR(255) NOT NULL,
+        description TEXT,
+        is_received ENUM('yes', 'no') DEFAULT 'no',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        informant_id VARCHAR(36),
+        room_id VARCHAR(10),
+        FOREIGN KEY (informant_id) REFERENCES _User(id),
+        FOREIGN KEY (room_id) REFERENCES ${roomTableName}(id)
+    )`;
+    await pool.promise().query(createMailTableQuery);
+
+
+}
+
 // CREATE
 router.post('/organizations', async (req, res) => {
     const { name, street_address, city, state_province, postal_code, country, email, phone, description } = req.body;
@@ -24,47 +96,7 @@ router.post('/organizations', async (req, res) => {
 
         const query = 'INSERT INTO _Organization (org_code, name, street_address, city, state_province, postal_code, country, email, phone, description) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)';
         await pool.promise().query(query, [org_code, name, street_address, city, state_province, postal_code, country, email, phone, description]); // Using promise-based version
-        
-        // Create the bill table associated with the organization
-        const billTableName = `${org_code}_bill`;
-        const createBillTableQuery = `CREATE TABLE ${billTableName} (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            title VARCHAR(255) NOT NULL,
-            description TEXT,
-            cost DOUBLE,
-            is_paid ENUM('yes', 'no') DEFAULT 'no',
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            customer_id VARCHAR(36),
-            informant_id VARCHAR(36),
-            FOREIGN KEY (informant_id) REFERENCES _User(id),
-            FOREIGN KEY (customer_id) REFERENCES _User(id)
-        )`;
-        await pool.promise().query(createBillTableQuery);
-
-        const announcementTableName = `${org_code}_announcement`;
-        const createAnnouncementTableQuery = `CREATE TABLE ${announcementTableName} (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            title VARCHAR(255) NOT NULL,
-            description TEXT,
-            is_expired ENUM('yes', 'no') DEFAULT 'no',
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            informant_id VARCHAR(36),
-            FOREIGN KEY (informant_id) REFERENCES _User(id)
-        )`;
-        await pool.promise().query(createAnnouncementTableQuery);
-
-        const maintenanceTableName = `${org_code}_maintenance`;
-        const createMaintenanceTableQuery = `CREATE TABLE ${maintenanceTableName} (
-            id INT AUTO_INCREMENT PRIMARY KEY,
-            title VARCHAR(255) NOT NULL,
-            description TEXT,
-            is_fixed ENUM('yes', 'no') DEFAULT 'no',
-            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-            informant_id VARCHAR(36),
-            FOREIGN KEY (informant_id) REFERENCES _User(id)
-        )`;
-        await pool.promise().query(createMaintenanceTableQuery);
-
+        await createOrganizationTables(org_code);
         return res.status(200).json({ message: 'Organization registered successfully' });
     } catch (err) {
         console.error('Error executing SQL query:', err);
@@ -105,18 +137,15 @@ router.delete('/organizations/:org_code', async (req, res) => {
     const org_code = req.params.org_code;
 
     try {
+        // Update org_code to null in _User table
+        await pool.promise().query('UPDATE _User SET org_code = NULL WHERE org_code = ?', [org_code]);
+
         // Drop the bill table associated with the organization
         const billTableName = `${org_code}_bill`;
         const maintenanceTableName = `${org_code}_maintenance`;
         const announcementTableName = `${org_code}_announcement`;
         await pool.promise().query(`DROP TABLE IF EXISTS \`${billTableName}\``);
-
-        // Drop the announcement table associated with the organization
-
         await pool.promise().query(`DROP TABLE IF EXISTS \`${announcementTableName}\``);
-
-        // Drop the maintenance table associated with the organization
-
         await pool.promise().query(`DROP TABLE IF EXISTS \`${maintenanceTableName}\``);
 
         // Finally, delete the organization itself
@@ -129,5 +158,6 @@ router.delete('/organizations/:org_code', async (req, res) => {
         return res.status(500).json({ error: 'Database error' });
     }
 });
+
 
 module.exports = { router }; 
